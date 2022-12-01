132ebebd3cfa2b94f1cd453e15b3d5efeb33ce68
Max Zhilin <max_zhilin@mail.ru>
2022-12-01 20:12:12 +0300

    Day 1

--- src/Day01.kt
+++ src/Day01.kt
@@ -1,15 +1,35 @@
 fun main() {
     fun part1(input: List<String>): Int {
-        return input.size
+        val list = mutableListOf<Int>()
+        list.add(0)
+        for (s in input) {
+            if (s.isEmpty()) {
+                list.add(0)
+            } else {
+                list[list.lastIndex]+= s.toInt()
+            }
+        }
+        return list.max()
     }
 
     fun part2(input: List<String>): Int {
-        return input.size
+        val list = mutableListOf<Int>()
+        list.add(0)
+        for (s in input) {
+            if (s.isEmpty()) {
+                list.add(0)
+            } else {
+                list[list.lastIndex]+= s.toInt()
+            }
+        }
+        list.sortDescending()
+        return list.take(3).sum()
     }
 
     // test if implementation meets criteria from the description, like:
     val testInput = readInput("Day01_test")
-    check(part1(testInput) == 1)
+    check(part1(testInput) == 24000)
+    check(part2(testInput) == 45000)
 
     val input = readInput("Day01")
     println(part1(input))
