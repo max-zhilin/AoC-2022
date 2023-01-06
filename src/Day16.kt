b1b563bc86a9e57c12199cd5bdaf4afd67e33e57
Max Zhilin <max_zhilin@mail.ru>
2023-01-06 17:52:27 +0500

    50*

--- src/Day16.kt
+++ src/Day16.kt
@@ -1,7 +1,11 @@
 import java.util.*
 
-data class Valve(val name: String, val rate: Int, val names: List<String>, var opened: Boolean = false, var lastRateA: Int = -1, var lastRateB: Int = -1) {
-    val list: MutableList<Pair<Valve, Int>> = mutableListOf()
+val solutions = mutableMapOf<Int, Int>()
+
+data class Link(val valve: Valve, val dist: Int)
+data class Valve(val name: String, val rate: Int, var opened: Boolean = false, var lastRateA: Int = -1, var lastRateB: Int = -1) {
+    lateinit var names: List<String>
+    val links: MutableList<Link> = mutableListOf()
     var lastVisitedRate: Int = -1
     companion object {
         fun parse(s: String): Valve {
@@ -16,27 +20,29 @@ data class Valve(val name: String, val rate: Int, val names: List<String>, var o
                 names.add(scanner.next())
             }
 
-            return Valve(name, rate, names)
+            val valve = Valve(name, rate)
+            valve.names = names
+            return valve
         }
     }
 }
 fun MutableList<Valve>.fillList() {
     forEach { v ->
         v.names.forEach { name ->
-            v.list.add(Pair(find { it.name == name }!!, 1))
+            v.links.add(Link(find { it.name == name }!!, 1))
         }
     }
 }
 fun MutableList<Valve>.optimize() {
     do {
         val v = find { it.rate == 0 && it.name != "AA" } ?: return
-        val leads = filter { it.list.map { it.first }.contains(v) }
+        val leads = filter { it.links.map { it.valve }.contains(v) }
         leads.forEach { lead ->
-            val link = lead.list.first { it.first == v }
-            lead.list.remove(link)
-            val listCopy = v.list.filter { it.first != lead }.toMutableList()
-            for ((i, e) in listCopy.withIndex()) listCopy[i] = Pair(e.first, e.second + link.second)
-            lead.list.addAll(listCopy)
+            val link = lead.links.first { it.valve == v }
+            lead.links.remove(link)
+            val listCopy = v.links.filter { it.valve != lead }.toMutableList()
+            for ((i, e) in listCopy.withIndex()) listCopy[i] = Link(e.valve, e.dist + link.dist)
+            lead.links.addAll(listCopy)
         }
         remove(v)
     } while (true)
@@ -54,8 +60,8 @@ fun Valve.maxPressure(valves: MutableList<Valve>, time: Int, releasedRate: Int):
     if (!opened) {
         val lastClosedRate = lastVisitedRate
         lastVisitedRate = releasedRate
-        list.forEach { node ->
-            foundSubPressure = maxOf(foundSubPressure, node.first.maxPressure(valves, time - node.second, releasedRate))
+        links.forEach { link ->
+            foundSubPressure = maxOf(foundSubPressure, link.valve.maxPressure(valves, time - link.dist, releasedRate))
         }
         lastVisitedRate = lastClosedRate
         if (name == "AA") return foundSubPressure
@@ -71,109 +77,202 @@ fun Valve.maxPressure(valves: MutableList<Valve>, time: Int, releasedRate: Int):
 
         val lastOpenedRate = lastVisitedRate
         lastVisitedRate = releasedRate
-        list.forEach { node ->
-            foundSubPressure = maxOf(foundSubPressure, additionalPressure + node.first.maxPressure(valves, time - 1 - node.second, releasedRate + rate))
+        links.forEach { link ->
+            foundSubPressure = maxOf(foundSubPressure, additionalPressure + link.valve.maxPressure(valves, time - 1 - link.dist, releasedRate + rate))
         }
         lastVisitedRate = lastOpenedRate
         opened = false
     } else /* opened */ {
         val lastOpenedRate = lastVisitedRate
         lastVisitedRate = releasedRate
-        list.forEach { node ->
-            foundSubPressure = maxOf(foundSubPressure, node.first.maxPressure(valves, time - node.second, releasedRate))
+        links.forEach { link ->
+            foundSubPressure = maxOf(foundSubPressure, link.valve.maxPressure(valves, time - link.dist, releasedRate))
         }
         lastVisitedRate = lastOpenedRate
     }
 
     return foundSubPressure
 }
-fun MutableList<Valve>.maxPressure(nodeA: Pair<Valve, Int>, nodeB: Pair<Valve, Int>, time: Int, releasedRate: Int, counter: Int): Int {
+fun MutableList<Valve>.maxPressure(linkA: Link, linkB: Link, time: Int, releasedRateA: Int, releasedRateB: Int, depth: Int, result: Int): Int {
 
-    val a = nodeA.first // target valve
-    val b = nodeB.first
-    val spentTime = minOf(nodeA.second, nodeB.second) // time to target (1+)
+    val a = linkA.valve // target valve
+    val b = linkB.valve
+    val spentTime = minOf(linkA.dist, linkB.dist) // time to target (1+)
     val timeLeft = time - spentTime
-    val waitA = nodeA.second - spentTime // 0+
-    val waitB = nodeB.second - spentTime
+    val waitA = linkA.dist - spentTime // 0+
+    val waitB = linkB.dist - spentTime
 
 //    println("  ".repeat(counter) + "${a.name} ${b.name} time $timeLeft rate $releasedRate    ${this.filter { it.opened }.joinToString { it.name }}")
 
     // time is out
     if (timeLeft < 2) return 0 // it takes min 2 to open and release
+    if (timeLeft < 14) {
+//        if (solutions[result] == null) solutions[result] = 1
+//        else solutions[result] = solutions[result]!! + 1
+        if (result < 2000) return 0
+    }
 
     // last...Rate - prevent cycling
-    if (a.lastRateA == releasedRate || b.lastRateB == releasedRate) {
-//        println("  ".repeat(counter) + "ret lastRate ${if (a.lastRateA == releasedRate) "a" else ""} ${if (b.lastRateB == releasedRate) "b" else ""}")
-        return 0
-    }
+//    if (a.lastRateA == releasedRateA || b.lastRateB == releasedRateB) {
+//        // TODO: remove
+////        println("  ".repeat(counter) + "ret lastRate ${if (a.lastRateA == releasedRate) "a" else ""} ${if (b.lastRateB == releasedRate) "b" else ""}")
+//        return 0
+//    }
 
     var foundSubPressure = 0
 
     val tempRateA = a.lastRateA
     val tempRateB = b.lastRateB
-//    val tempRateB = b.lastRateA
     if (waitA == 0)
-        a.lastRateA = releasedRate
+        a.lastRateA = releasedRateA
     if (waitB == 0)
-        b.lastRateB = releasedRate
-//    b.lastRateA = releasedRate
+        b.lastRateB = releasedRateB
 
-//    println(" ".repeat(counter) + "${a.name} ${b.name} $timeLeft $releasedRate    ${this.filter { it.opened }.joinToString { it.name }}")
-    if (a != b && !a.opened && waitA == 0 && a.name != "AA" && !b.opened && waitB == 0 && b.name != "AA") {
-        a.opened = true
-        b.opened = true
-        val additionalPressure = (a.rate + b.rate) * (timeLeft - 1)
-        if (all { it.opened || it.name == "AA" }) {
-            a.opened = false
+    if (waitA > 0) {
+        if (!b.opened && b.name != "AA") { // closed & !AA so we open
+            val openedPressure = b.rate * (timeLeft - 1)
+            if (all { it.opened || it.name == "AA" || it == b }) {
+                a.lastRateA = tempRateA
+                b.lastRateB = tempRateB
+                return openedPressure
+            }
+            b.opened = true
+            b.lastRateB = releasedRateB + b.rate
+            for (linkB in b.links)
+                foundSubPressure = maxOf(foundSubPressure,
+                    openedPressure + maxPressure(Link(a, waitA - 1), linkB, timeLeft - 1, releasedRateA, releasedRateB + b.rate, depth + 1, openedPressure + result))
             b.opened = false
-            a.lastRateA = tempRateA
-            b.lastRateB = tempRateB
-//            b.lastRateA = tempRateB
-            return additionalPressure
+            b.lastRateB = releasedRateB
         }
-        foundSubPressure = maxOf(foundSubPressure, additionalPressure + maxPressure(Pair(a, 0), Pair(b, 0), timeLeft - 1, releasedRate + a.rate + b.rate, counter + 1))
-        a.opened = false
-        b.opened = false
-    }
-    if (!a.opened && waitA == 0 && a.name != "AA") {
-        a.opened = true
-        val additionalPressure = a.rate * (timeLeft - 1)
-        if (all { it.opened || it.name == "AA" }) {
-            a.opened = false
-            a.lastRateA = tempRateA
-            b.lastRateB = tempRateB
-//            b.lastRateA = tempRateB
-            return additionalPressure
-        }
-        for (node in b.list)
-            foundSubPressure = maxOf(foundSubPressure, additionalPressure + maxPressure(Pair(a, 0), node, timeLeft - 1, releasedRate + a.rate, counter + 1))
-        a.opened = false
-    }
-    if (!(a == b && waitA == 0) && !b.opened && waitB == 0 && b.name != "AA") {
-        b.opened = true
-        val additionalPressure = b.rate * (timeLeft - 1)
-        if (all { it.opened || it.name == "AA" }) {
-            b.opened = false
-            a.lastRateA = tempRateA
-            b.lastRateB = tempRateB
-//            b.lastRateA = tempRateB
-            return additionalPressure
-        }
-        for (node in a.list)
-            foundSubPressure = maxOf(foundSubPressure, additionalPressure + maxPressure(node, Pair(b, 0), timeLeft - 1, releasedRate + b.rate, counter + 1))
-        b.opened = false
-    }
+        val links = b.links.filter { it.valve.lastRateB < releasedRateB }
+        if (links.isEmpty() && b.opened)
+            foundSubPressure = maxOf(foundSubPressure, maxPressure(Link(a, waitA), Link(b, 1000), timeLeft, releasedRateA, releasedRateB, depth + 1, result))
+        else
+            for (linkB in links)
+                foundSubPressure = maxOf(foundSubPressure, maxPressure(Link(a, waitA), linkB, timeLeft, releasedRateA, releasedRateB, depth + 1, result))
 
-    if (waitA > 0)
-        for (node in b.list)
-            foundSubPressure = maxOf(foundSubPressure, maxPressure(Pair(a, waitA), node, timeLeft, releasedRate, counter + 1))
-    else if (waitB > 0)
-        for (node in a.list)
-            foundSubPressure = maxOf(foundSubPressure, maxPressure(node, Pair(b, waitB), timeLeft, releasedRate, counter + 1))
-    else // waitA & waitB = 0
-        for (nodeA in a.list)
-            for (nodeB in b.list)
-                foundSubPressure = maxOf(foundSubPressure, maxPressure(nodeA, nodeB, timeLeft, releasedRate, counter + 1))
+    } else if (waitB > 0) {
+        if (!a.opened && a.name != "AA") { // closed && !AA so we open
+            val openedPressure = a.rate * (timeLeft - 1)
+            if (all { it.opened || it.name == "AA" || it == a }) {
+                a.lastRateA = tempRateA
+                b.lastRateB = tempRateB
+                return openedPressure
+            }
+            a.opened = true
+            a.lastRateA = releasedRateA + a.rate
+            for (linkA in a.links)
+                foundSubPressure = maxOf(foundSubPressure,
+                    openedPressure + maxPressure(linkA, Link(b, waitB - 1), timeLeft - 1, releasedRateA + a.rate, releasedRateB, depth + 1, openedPressure + result))
+            a.opened = false
+            a.lastRateA = releasedRateA
+        }
+        val links = a.links.filter { it.valve.lastRateA < releasedRateA }
+        if (links.isEmpty() && a.opened)
+            foundSubPressure = maxOf(foundSubPressure, maxPressure(Link(a, 1000), Link(b, waitB), timeLeft, releasedRateA, releasedRateB, depth + 1, result))
+        else
+            for (linkA in links)
+                foundSubPressure = maxOf(foundSubPressure, maxPressure(linkA, Link(b, waitB), timeLeft, releasedRateA, releasedRateB, depth + 1, result))
+    } else // waitA & waitB = 0
+        if (a == b) {
+            if (!a.opened && a.name != "AA") { //
+                val openedPressure = a.rate * (timeLeft - 1)
+                if (all { it.opened || it.name == "AA" || it == a }) {
+                    a.lastRateA = tempRateA
+                    b.lastRateB = tempRateB
+                    return openedPressure
+                }
+                a.opened = true // a open, b pass
+                a.lastRateA = releasedRateA + a.rate
+                val links = b.links.filter { it.valve.lastRateB < releasedRateB }
+                if (links.isEmpty())
+                    foundSubPressure = maxOf(foundSubPressure,
+                        openedPressure + maxPressure(Link(a, 0), Link(b, 1000), timeLeft - 1, releasedRateA + a.rate, releasedRateB, depth + 1, openedPressure + result))
+                else
+                    for (linkB in links)
+                        foundSubPressure = maxOf(foundSubPressure,
+                            openedPressure + maxPressure(Link(a, 0), Link(linkB.valve, linkB.dist - 1), timeLeft - 1, releasedRateA + a.rate, releasedRateB, depth + 1, openedPressure + result))
+                a.opened = false
+                a.lastRateA = releasedRateA
+            }
+            // both pass thru
+            val linksA = a.links.filter { it.valve.lastRateA < releasedRateA }
+            val linksB = b.links.filter { it.valve.lastRateB < releasedRateB }
+            if (linksA.isEmpty())
+                if (linksB.isEmpty())
+//                    return 0
+                else
+                    for (linkB in linksB)
+                        foundSubPressure = maxOf(foundSubPressure,
+                            maxPressure(Link(a, 1000), linkB, timeLeft, releasedRateA, releasedRateB, depth + 1, result))
+            else
+                for (linkA in linksA)
+                    if (linksB.isEmpty())
+                        foundSubPressure = maxOf(foundSubPressure,
+                            maxPressure(linkA, Link(b, 1000), timeLeft, releasedRateA, releasedRateB, depth + 1, result))
+                    else
+                        for (linkB in linksB)
+                            foundSubPressure = maxOf(foundSubPressure,
+                                maxPressure(linkA, linkB, timeLeft, releasedRateA, releasedRateB, depth + 1, result))
+
+        } else { // a <> b
+            if (!a.opened && a.name != "AA") {
+                val openedPressureA = a.rate * (timeLeft - 1)
+                if (all { it.opened || it.name == "AA" || it == a }) {
+                    a.lastRateA = tempRateA
+                    b.lastRateB = tempRateB
+                    return openedPressureA
+                }
+                a.opened = true
+                a.lastRateA = releasedRateA + a.rate
+
+                if (!b.opened && b.name != "AA") {
+                    val openedPressureB = b.rate * (timeLeft - 1)
+                    if (all { it.opened || it.name == "AA" || it == b }) {
+                        a.opened = false
+                        a.lastRateA = tempRateA
+                        b.lastRateB = tempRateB
+                        return openedPressureA + openedPressureB
+                    }
+                    b.opened = true
+                    b.lastRateB = releasedRateB + b.rate
+
+                    foundSubPressure = maxOf(foundSubPressure,
+                        openedPressureA + openedPressureB + maxPressure(Link(a, 0), Link(b, 0), timeLeft - 1, releasedRateA + a.rate, releasedRateB + b.rate, depth + 1, openedPressureA + openedPressureB + result))
+                    b.opened = false
+                    b.lastRateB = releasedRateB
+                }
+
+                val links = b.links.filter { it.valve.lastRateB < releasedRateB }
+                if (links.isEmpty())
+                    foundSubPressure = maxOf(foundSubPressure,
+                        openedPressureA + maxPressure(Link(a, 0), Link(b, 1000), timeLeft - 1, releasedRateA + a.rate, releasedRateB, depth + 1, openedPressureA + result))
+                else
+                    for (linkB in links)
+                        foundSubPressure = maxOf(foundSubPressure,
+                            openedPressureA + maxPressure(Link(a, 0), Link(linkB.valve, linkB.dist - 1), timeLeft - 1, releasedRateA + a.rate, releasedRateB, depth + 1, openedPressureA + result))
+                a.opened = false
+                a.lastRateA = releasedRateA
+            }
+            val linksA = a.links.filter { it.valve.lastRateA < releasedRateA }
+            val linksB = b.links.filter { it.valve.lastRateB < releasedRateB }
+            if (linksA.isEmpty())
+                if (linksB.isEmpty())
+//                    return 0
+                else
+                    for (link in linksB)
+                        foundSubPressure = maxOf(foundSubPressure,
+                            maxPressure(Link(a, 1000), link, timeLeft, releasedRateA, releasedRateB, depth + 1, result))
+            else
+                for (linkA in linksA)
+                    if (linksB.isEmpty())
+                        foundSubPressure = maxOf(foundSubPressure,
+                            maxPressure(linkA, Link(b, 1000), timeLeft, releasedRateA, releasedRateB, depth + 1, result))
+                    else
+                        for (linkB in linksB)
+                            foundSubPressure = maxOf(foundSubPressure,
+                                maxPressure(linkA, linkB, timeLeft, releasedRateA, releasedRateB, depth + 1, result))
+        }
 
     a.lastRateA = tempRateA
     b.lastRateB = tempRateB
@@ -204,20 +303,21 @@ fun main() {
 
         valves.optimize()
 
-        val rootNode = valves.first {it.name == "AA" } to 0
-        return valves.maxPressure(rootNode, rootNode, time, 0, 0)
+        val rootNode = Link(valves.first {it.name == "AA" }, 0)
+        return valves.maxPressure(rootNode, rootNode, time - 0, 0, 0, 0, 0)
     }
 
     // test if implementation meets criteria from the description, like:
-//    val testInput = readInput("Day16_test")
+    val testInput = readInput("Day16_test")
 //    val testInput = readInput("Day16_test2")
-    val testInput = readInput("Day16_test3")
+//    val testInput = readInput("Day16_test3")
 //    println(part1(testInput))
 //    check(part1(testInput, 30) == 1651)
-    println(part2(testInput, 26))
-//    check(part2(testInput, 26) == 1707)
+//    println(part2(testInput, 26))
+//    check(part2(testInput, 26) == 1707) //1652
 
     val input = readInput("Day16")
 //    println(part1(input, 30))
-//    println(part2(input, 26))
+    println(part2(input, 26)) // 2772
+//    println(solutions.map { (key, value) -> Pair(key, value) }.sortedBy { it.first }.joinToString("\n"))
 }
